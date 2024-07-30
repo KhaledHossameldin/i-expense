8e05d98972002e188a607a5778def1a94d9bd999
Khaled Hossameldin <khaled-hossam@outlook.com>
2024-07-30 21:02:52 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Khaled Hossameldin on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
